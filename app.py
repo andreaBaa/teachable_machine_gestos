78891c3daa93d943186d485fc13d86000483e99c
andreaBaa <160878920+andreaBaa@users.noreply.github.com>
2024-04-11 10:37:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ image2 = Image.open('rock.jpg')
 st.image(image2, width=200)
 
 
-img_file_buffer = st.camera_input("Toma una Foto")
+img_file_buffer = st.camera_input("Toma la foto aquí:")
 
 if img_file_buffer is not None:
     # To read image file buffer with OpenCV:
